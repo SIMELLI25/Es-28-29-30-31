fe93d015a50c6827b3d6ab49a9ad95906c15b294
unknown <simomelli2005@gmail.com>
2020-12-01 15:30:06 +0100

    aggiungo all'es 28 la parte con le liste a casa

--- es28.py
+++ es28.py
@@ -1,4 +1,7 @@
 input("ES 28")
+
+input("Con i dizionari")
+
 dizionario = {}
 nrag = int(input("quanti ragazzi partecipano?"))
 count = 0
@@ -12,3 +15,21 @@ while True:
         dizionario.update({lan : rag})
 top = max(dizionario)
 print("il punteggio migliore è", top)
+
+input("Con le liste")
+
+lanci = []
+count = 1
+cand_e_lan = []
+while True:
+    cand = input("inserisci nome candidato: ")
+    lan = input("inserisci la misura del lancio: ")
+    cand_e_lan.append(cand)
+    cand_e_lan.append(lan)
+    lanci.append(lan)
+    count = count + 1
+    x = int(input("se vuoi inserire altri candidato digita 1 altrimenti se termini qui digita 0: "))
+    if x == 0:
+        break
+lanci.sort(reverse = True, key = int)
+print("il vincitore è", cand_e_lan[cand_e_lan.index(lanci[0]) - 1], "con un lancio di", lanci[0], "metri")
